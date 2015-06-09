637dc87b0f96377ec0a1a5523dd9a68f434dd0ab
Michael Ripley <zkxs00@gmail.com>
2015-06-09 11:06:56 -0500

    Documented NetflowEntry.java

--- src/netflow/NetflowEntry.java
+++ src/netflow/NetflowEntry.java
@@ -1,8 +1,15 @@
 package netflow;
 
+/**
+ * Stores the data for a single netflow entry. Some datagrams may contain multiple entries,
+ * so a linked list structure using {@link #nextEntry} is created.
+ * @author Michael Ripley (<a href="mailto:michael-ripley@utulsa.edu">michael-ripley@utulsa.edu</a>) Jun 9, 2015
+ */
 public class NetflowEntry
 {	
+	/** The next netflow entry from a single datagram, or <code>null</code> if there are no more */
 	private NetflowEntry nextEntry;
+	
 	private int version;
 	private int count;
 	private long sys_uptime;
