a682553a66db3d8461fe6ce4333a1141b369b244
Michael Ripley <zkxs00@gmail.com>
2015-06-11 13:57:07 -0500

    Added an exception to be thrown on invalid packets

---
+++ src/netflow/processing/InvalidPacketException.java
@@ -0,0 +1,18 @@
+package netflow.processing;
+
+/**
+ * Thrown when a packet is invalid and could not be parsed
+ * @author Michael Ripley (<a href="mailto:michael-ripley@utulsa.edu">michael-ripley@utulsa.edu</a>) Jun 11, 2015
+ */
+public class InvalidPacketException extends Exception
+{
+	public InvalidPacketException()
+	{
+		super();
+	}
+
+	public InvalidPacketException(String message)
+	{
+		super(message);
+	}	
+}
