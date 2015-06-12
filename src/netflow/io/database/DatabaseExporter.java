ba89b67ade941caf916d147b12676e30c24d012e
Michael Ripley <zkxs00@gmail.com>
2015-06-12 14:04:30 -0500

    Made empty DatabaseExporter class

--- src/netflow/io/database/DatabaseExporter.java
+++ src/netflow/io/database/DatabaseExporter.java
@@ -1,6 +1,21 @@
 package netflow.io.database;
 
-public class DatabaseExporter
-{	
+import netflow.NetflowEntry;
+import netflow.io.Exporter;
+
+public class DatabaseExporter implements Exporter
+{
+	public DatabaseExporter()
+	{
+		
+	}
+	
+	
+	@Override
+	public void Export(NetflowEntry netflow)
+	{
+		// TODO Auto-generated method stub
+		
+	}	
 	
 }
